8aa43e78762a51b5f57923e3d48462352d0cfd3f
G13 ReqJing23 <3315651144@qq,com>
2025-11-12 17:23:48 +0800

    增加会话机制框架

--- Lab1/include/Common.h
+++ Lab1/include/Common.h
@@ -22,9 +22,6 @@ std::string buildMessage(const Message& m) ;
 //解析消息
 Message parseMessage(const std::string& strMsg) ;
 
-<<<<<<< HEAD
 //定义消息类型
 enum TYPE{SYS, JOIN , MSG ,EXIT, SWITCH_CHAT};
-=======
->>>>>>> d75a0fb4afe60d6c9a761753aedec4a22577c70c
 #endif // COMMON_H
